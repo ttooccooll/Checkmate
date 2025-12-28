51dc07d987752e50a573b1db5838c1d456d0947a
Jason <jasonbohio@gmail.com>
2025-12-27 19:51:28 -0500

    shadowblur

--- script.js
+++ script.js
@@ -1034,7 +1034,7 @@ function spawnDust() {
   }
 
   // --- Cap the array length ---
-  const MAX_DUST = 60;
+  const MAX_DUST = 80;
   if (dustParticles.length > MAX_DUST) {
     dustParticles.splice(0, dustParticles.length - MAX_DUST);
   }
