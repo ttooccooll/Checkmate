7df1ee64cb37d03f5bbc1510589c84db7c81e5c7
Jason <jasonbohio@gmail.com>
2025-12-27 21:19:18 -0500

    services

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,13 +1,13 @@
-const WORLD_WIDTH = 3000;
-const WORLD_HEIGHT = 3000;
-const ROAD_HEIGHT = 100;
-const COLLISION_FACTOR = 0.5;
-const ROAD_BUFFER = 10;
+export const WORLD_WIDTH = 3000;
+export const WORLD_HEIGHT = 3000;
+export const ROAD_HEIGHT = 100;
+export const COLLISION_FACTOR = 0.5;
+export const ROAD_BUFFER = 10;
 
-const PLAYER_WIDTH = 60;
-const PLAYER_HEIGHT = 55;
+export const PLAYER_WIDTH = 60;
+export const PLAYER_HEIGHT = 55;
 
-const INVULNERABLE_DURATION = 80;
-const FLASH_DURATION = 150;
+export const INVULNERABLE_DURATION = 80;
+export const FLASH_DURATION = 150;
 
-const OFFROAD_MAX = 1200;
+export const OFFROAD_MAX = 1200;
