1e6700ba57cae5a8222ede74e7ae44d4bf98d47c
Jason <jasonbohio@gmail.com>
2026-01-06 09:53:31 -0500

    test

--- src/entities/npcs.js
+++ src/entities/npcs.js
@@ -51,7 +51,6 @@ export class NPC {
     if (now - this.lastTalkTime < 500) return false;
 
     if (this.hasTalked) return false;
-    this.hasTalked = true;
 
     let lines = [...this.dialogQueue];
 
